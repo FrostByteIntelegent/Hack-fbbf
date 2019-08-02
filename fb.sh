bd4d18a85ced6394e7c478401b961e4f6cd99211
FrostByteIntelegent <53590567+FrostByteIntelegent@users.noreply.github.com>
2019-08-02 10:15:51 +0700

    Add files via upload

---
+++ fb.sh
@@ -0,0 +1,6 @@
+echo "Hallo apa kah kamu ingin menggunakan script hack fb? "
+read fb
+echo "apakah kamu sudah siap? "
+echo "tools akan di mulai dalam 20 detik"
+hackfb.py
+echo "seperti nya gagal membuka script"
